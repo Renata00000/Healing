a54fb6e098d50f6ca2a3c3eedf21139cc7387ebb
Renata <renata0cristiane@gmail.com>
2024-06-10 12:38:43 -0300

    atualizando manage.py

--- manage.py
+++ manage.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+# import os
+# import sys
+
+
+# def main():
+#     """Run administrative tasks."""
+#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healing.settings')
+#     try:
+#         from django.core.management import execute_from_command_line
+#     except ImportError as exc:
+#         raise ImportError(
+#             "Couldn't import Django. Are you sure it's installed and "
+#             "available on your PYTHONPATH environment variable? Did you "
+#             "forget to activate a virtual environment?"
+#         ) from exc
+#     execute_from_command_line(sys.argv)
+
+
+# if __name__ == '__main__':
+#     main()
+
+
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healing.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+  """Run administrative tasks."""
+  os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healing.settings')
+  try:
+    from django.core.management import execute_from_command_line
+  except ImportError as exc:
+    raise ImportError(
+      "Couldn't import Django. Are you sure it's installed and "
+      "available on your PYTHONPATH environment variable? Did you "
+      "forget to activate a virtual environment?"
+    ) from exc
+  execute_from_command_line(sys.argv)
+
+
+from django.core.wsgi import get_wsgi_application
+
+def handler(event, context):
+    application = get_wsgi_application()
+    response = application(event, context)
+    return response
 
 
 if __name__ == '__main__':
-    main()
+  main()
